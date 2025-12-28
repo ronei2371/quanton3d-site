6a2c590f751f7a73718f20cbe02991181591b695
Ronei Martins Fonseca <ronei3271@gmail.com>
2025-12-27 23:57:20 -0300

    chore: update render hosting setup

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,17 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  appType: 'spa',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
     },
   },
 })
